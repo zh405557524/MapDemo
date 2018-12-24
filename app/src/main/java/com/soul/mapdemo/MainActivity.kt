da910ab9c978ce2403bad3d302214c91bb4a5443
zh405557524 <405557524@qq.com>
2018-12-24 16:06:55 +0800

    第一次提交

---
+++ app/src/main/java/com/soul/mapdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.soul.mapdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
